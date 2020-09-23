ab8c7c91c183e74a53325f29dd131bb23e8d2f33
sungwoo1548 <swo1548@gmail.com>
2020-09-24 00:09:01 +0900

    redux, react-redux 적용 예제

--- src/index.js
+++ src/index.js
@@ -3,4 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import rootReducer from './modules';
+
+const store = createStore(rootReducer);
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+    , document.getElementById('root'));
--- src/pages/router/Routes.js
+++ src/pages/router/Routes.js
@@ -3,6 +3,7 @@ import { Switch, Route } from 'react-router-dom';
 
 import './Routes.css';
 
+import Home from '../main/home/Home';
 import ChannelRanking from '../main/ChannelRanking';
 import TodayVideos from '../main/TodayVideos';
 import PostList from '../main/post/PostList';
@@ -11,7 +12,7 @@ export default function Routes() {
   return (
     <>
       <Switch>
-        <Route exact path="/" render={() => <div>This is greeting page</div>} />
+        <Route exact path="/" render={() => <Home />} />
         <Route path="/channelranking" render={() => <ChannelRanking />} />
         <Route path="/todayvideos" render={() => <TodayVideos />} />
         <Route path="/post/list" render={() => <PostList />} />
